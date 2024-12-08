66702b7376b033ed8a4489d79f83ce8d67b11c0f
Shanilka Srimal <141924142+SanilkaSrimal@users.noreply.github.com>
2024-12-08 12:16:48 +0530

    Add files via upload

---
+++ connection/connection.php
@@ -0,0 +1,17 @@
+<?php
+
+// Database connection settings
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "themiya";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
